ec4f0c1bd9fb545fd6506b018ddb149609b89c37
Will Townsend <will@townsend.io>
2026-05-19 10:36:59 -0700

    fix: harden scalar aggregate support

--- lib/aggregate.ex
+++ lib/aggregate.ex
@@ -16,12 +16,7 @@ defmodule AshSqlite.Aggregate do
   def relationship_filter_uses_parent?(%{filter: nil}), do: false
 
   def relationship_filter_uses_parent?(%{filter: filter}) do
-    filter
-    |> Ash.Filter.flat_map(fn
-      %Ash.Query.Parent{} -> [:parent]
-      _ -> []
-    end)
-    |> Enum.any?()
+    filter_uses_parent?(filter)
   end
 
   defp do_add_aggregates(query, [], _resource, _select?), do: {:ok, query}
@@ -38,11 +33,29 @@ defmodule AshSqlite.Aggregate do
          "AshSqlite only supports loading related count, sum, avg, min, max and exists aggregates"}
 
       true ->
-        aggregates
-        |> Enum.group_by(& &1.relationship_path)
-        |> Enum.reduce_while({:ok, query, []}, fn {relationship_path, aggregates},
-                                                  {:ok, query, dynamics} ->
-          case add_aggregate_group(query, resource, relationship_path, aggregates) do
+        {already_added, remaining} =
+          aggregates
+          |> Enum.uniq_by(& &1.name)
+          |> Enum.split_with(&already_added?(&1, query.__ash_bindings__))
+
+        already_added_dynamics =
+          if select? do
+            Enum.map(already_added, &existing_aggregate_dynamic(&1, query.__ash_bindings__))
+          else
+            []
+          end
+
+        remaining
+        |> Enum.group_by(&aggregate_group_key/1)
+        |> Enum.reduce_while({:ok, query, already_added_dynamics}, fn {relationship_path,
+                                                                       aggregates},
+                                                                      {:ok, query, dynamics} ->
+          case add_aggregate_group(
+                 query,
+                 resource,
+                 aggregate_relationship_path(relationship_path),
+                 aggregates
+               ) do
             {:ok, query, new_dynamics} ->
               {:cont, {:ok, query, new_dynamics ++ dynamics}}
 
@@ -66,10 +79,54 @@ defmodule AshSqlite.Aggregate do
 
   defp supported?(%{kind: kind, related?: related?, relationship_path: path})
        when kind in [:count, :sum, :avg, :max, :min, :exists] do
-    related? != false && match?([_], path)
+    related? != false && match?([_ | _], path)
   end
 
   defp supported?(_), do: false
+
+  defp aggregate_group_key(aggregate) do
+    read_action = (aggregate.query.action && aggregate.query.action.name) || aggregate.read_action
+
+    {aggregate.relationship_path, read_action, aggregate.join_filters || %{},
+     aggregate_filter_group_key(aggregate)}
+  end
+
+  defp aggregate_relationship_path(
+         {relationship_path, _read_action, _join_filters, _aggregate_filter_group}
+       ) do
+    relationship_path
+  end
+
+  defp aggregate_filter_group_key(aggregate) do
+    if aggregate_filter_uses_relationships?(aggregate) do
+      {:filter, aggregate.name}
+    else
+      :shared
+    end
+  end
+
+  defp already_added?(aggregate, bindings) do
+    Enum.any?(bindings.bindings, fn
+      {_binding, %{type: :aggregate, aggregates: aggregates}} ->
+        aggregate.name in Enum.map(aggregates, & &1.name)
+
+      _binding ->
+        false
+    end)
+  end
+
+  defp existing_aggregate_dynamic(aggregate, bindings) do
+    {binding, _aggregate_binding} =
+      Enum.find(bindings.bindings, fn
+        {_binding, %{type: :aggregate, aggregates: aggregates}} ->
+          aggregate.name in Enum.map(aggregates, & &1.name)
+
+        _binding ->
+          false
+      end)
+
+    {aggregate.load, aggregate.name, loaded_aggregate_dynamic(aggregate, binding)}
+  end
 
   defp add_aggregate_group(query, resource, [relationship_name], aggregates) do
     relationship = Ash.Resource.Info.relationship(resource, relationship_name)
@@ -105,7 +162,8 @@ defmodule AshSqlite.Aggregate do
   defp do_add_aggregate_group(query, relationship, aggregates) do
     binding = query.__ash_bindings__.current
 
-    with {:ok, aggregate_query} <-
+    with :ok <- validate_aggregate_filters(aggregates),
+         {:ok, aggregate_query} <-
            aggregate_query(query, relationship, aggregates, binding) do
       aggregate_query = Ecto.Query.subquery(aggregate_query)
       root_binding = query.__ash_bindings__.root_binding
@@ -136,7 +194,8 @@ defmodule AshSqlite.Aggregate do
   end
 
   defp aggregate_query(parent_query, relationship, aggregates, binding) do
-    with {:ok, query} <- related_query(parent_query, relationship, hd(aggregates), binding) do
+    with {:ok, query} <-
+           related_query(parent_query, relationship, hd(aggregates), binding) do
       root_binding = query.__ash_bindings__.root_binding
 
       query =
@@ -165,6 +224,7 @@ defmodule AshSqlite.Aggregate do
     |> Ash.Query.unset([:filter, :sort, :distinct, :select, :limit, :offset])
     |> Ash.Query.set_context(relationship.context)
     |> Ash.Query.do_filter(relationship.filter, parent_stack: [relationship.source])
+    |> Ash.Query.do_filter(join_filter(aggregate, [relationship.name]))
     |> Ash.Query.set_context(%{
       data_layer: %{
         start_bindings_at: binding,
@@ -184,28 +244,179 @@ defmodule AshSqlite.Aggregate do
     end
   end
 
+  defp join_filter(%{join_filters: join_filters}, relationship_path)
+       when is_map(join_filters) do
+    Map.get(join_filters, relationship_path)
+  end
+
+  defp join_filter(_aggregate, _relationship_path), do: nil
+
+  defp validate_aggregate_filters(aggregates) do
+    cond do
+      Enum.any?(aggregates, &aggregate_filter_uses_parent?/1) ->
+        {:error,
+         "AshSqlite does not support loading aggregates with parent-dependent aggregate filters"}
+
+      Enum.any?(aggregates, &aggregate_filter_uses_parent_dependent_relationship?/1) ->
+        {:error,
+         "AshSqlite does not support loading aggregates with filters that reference relationships with parent-dependent filters"}
+
+      Enum.any?(aggregates, &aggregate_filter_uses_aggregates?/1) ->
+        {:error,
+         "AshSqlite does not support loading aggregates with aggregate filters that reference other aggregates"}
+
+      Enum.any?(aggregates, &unsupported_to_many_aggregate_filter?/1) ->
+        {:error,
+         "AshSqlite does not support loading sum, avg, or field-based count aggregates with filters that reference to-many relationships"}
+
+      Enum.any?(aggregates, &join_filters_use_parent?/1) ->
+        {:error,
+         "AshSqlite does not support loading aggregates with parent-dependent join filters"}
+
+      true ->
+        :ok
+    end
+  end
+
+  defp aggregate_filter_uses_parent?(%{query: %{filter: filter}}) do
+    filter_uses_parent?(filter)
+  end
+
+  defp aggregate_filter_uses_parent_dependent_relationship?(%{
+         query: %{filter: filter, resource: resource}
+       }) do
+    filter
+    |> aggregate_filter_relationship_paths()
+    |> Enum.any?(&parent_dependent_relationship_path?(resource, &1))
+  end
+
+  defp aggregate_filter_uses_parent_dependent_relationship?(_aggregate), do: false
+
+  defp aggregate_filter_uses_relationships?(%{query: %{filter: filter}}) do
+    filter
+    |> aggregate_filter_relationship_paths()
+    |> Enum.any?()
+  end
+
+  defp aggregate_filter_uses_relationships?(_aggregate), do: false
+
+  defp aggregate_filter_uses_aggregates?(%{query: %{filter: filter}}) when not is_nil(filter) do
+    filter
+    |> Ash.Filter.used_aggregates([])
+    |> Enum.any?()
+  end
+
+  defp aggregate_filter_uses_aggregates?(_aggregate), do: false
+
+  defp unsupported_to_many_aggregate_filter?(%{kind: :count, field: field} = aggregate)
+       when not is_nil(field) do
+    aggregate_filter_references_to_many_relationship?(aggregate) && !aggregate.uniq?
+  end
+
+  defp unsupported_to_many_aggregate_filter?(%{kind: kind} = aggregate)
+       when kind in [:sum, :avg] do
+    aggregate_filter_references_to_many_relationship?(aggregate)
+  end
+
+  defp unsupported_to_many_aggregate_filter?(_aggregate), do: false
+
+  defp aggregate_filter_references_to_many_relationship?(%{
+         query: %{filter: filter, resource: resource}
+       }) do
+    filter
+    |> aggregate_filter_relationship_paths()
+    |> Enum.any?(&to_many_relationship_path?(resource, &1))
+  end
+
+  defp aggregate_filter_references_to_many_relationship?(_aggregate), do: false
+
+  defp aggregate_filter_relationship_paths(nil), do: []
+
+  defp aggregate_filter_relationship_paths(%{expression: nil}), do: []
+
+  defp aggregate_filter_relationship_paths(filter) do
+    Ash.Filter.relationship_paths(filter)
+  end
+
+  defp parent_dependent_relationship_path?(_resource, []), do: false
+
+  defp parent_dependent_relationship_path?(resource, [relationship_name | rest]) do
+    case Ash.Resource.Info.relationship(resource, relationship_name) do
+      nil ->
+        false
+
+      relationship ->
+        relationship_filter_uses_parent?(relationship) ||
+          parent_dependent_relationship_path?(relationship.destination, rest)
+    end
+  end
+
+  defp to_many_relationship_path?(_resource, []), do: false
+
+  defp to_many_relationship_path?(resource, [relationship_name | rest]) do
+    case Ash.Resource.Info.relationship(resource, relationship_name) do
+      %{cardinality: :many} ->
+        true
+
+      nil ->
+        false
+
+      relationship ->
+        to_many_relationship_path?(relationship.destination, rest)
+    end
+  end
+
+  defp join_filters_use_parent?(%{join_filters: join_filters}) when is_map(join_filters) do
+    Enum.any?(join_filters, fn {_path, filter} -> filter_uses_parent?(filter) end)
+  end
+
+  defp join_filters_use_parent?(_aggregate), do: false
+
+  defp filter_uses_parent?(nil), do: false
+
+  defp filter_uses_parent?(%{expression: nil}), do: false
+
+  defp filter_uses_parent?(filter) do
+    Ash.Filter.find(
+      filter,
+      fn
+        %Ash.Query.Parent{} -> true
+        %Ash.Query.Call{name: :parent} -> true
+        _ -> false
+      end,
+      true,
+      true,
+      true
+    )
+    |> case do
+      nil -> false
+      _ -> true
+    end
+  end
+
   defp aggregate_dynamic(query, relationship, %{kind: :exists} = aggregate, binding) do
     count_field = count_field(relationship, aggregate)
     count_dynamic = Ecto.Query.dynamic(count(field(as(^binding), ^count_field)))
 
-    {query, count_dynamic} =
-      maybe_filter_aggregate(query, aggregate, count_dynamic)
-
-    {:ok, query, Ecto.Query.dynamic(^count_dynamic > 0)}
+    with {:ok, query, count_dynamic} <-
+           maybe_filter_aggregate(query, aggregate, count_dynamic) do
+      {:ok, query, Ecto.Query.dynamic(^count_dynamic > 0)}
+    end
   end
 
   defp aggregate_dynamic(query, relationship, %{kind: :count} = aggregate, binding) do
     count_field = count_field(relationship, aggregate)
 
     dynamic =
-      if aggregate.uniq? do
+      if count_distinct?(aggregate) do
         Ecto.Query.dynamic(count(field(as(^binding), ^count_field), :distinct))
       else
         Ecto.Query.dynamic(count(field(as(^binding), ^count_field)))
       end
 
-    {query, dynamic} = maybe_filter_aggregate(query, aggregate, dynamic)
-    {:ok, query, maybe_default_aggregate(dynamic, aggregate)}
+    with {:ok, query, dynamic} <- maybe_filter_aggregate(query, aggregate, dynamic) do
+      {:ok, query, maybe_default_aggregate(dynamic, aggregate)}
+    end
   end
 
   defp aggregate_dynamic(query, _relationship, aggregate, binding)
@@ -220,8 +431,9 @@ defmodule AshSqlite.Aggregate do
         :min -> Ecto.Query.dynamic(min(^field))
       end
 
-    {query, dynamic} = maybe_filter_aggregate(query, aggregate, dynamic)
-    {:ok, query, maybe_default_aggregate(dynamic, aggregate)}
+    with {:ok, query, dynamic} <- maybe_filter_aggregate(query, aggregate, dynamic) do
+      {:ok, query, maybe_default_aggregate(dynamic, aggregate)}
+    end
   end
 
   defp aggregate_dynamic(_query, _relationship, aggregate, _binding) do
@@ -243,25 +455,47 @@ defmodule AshSqlite.Aggregate do
     end
   end
 
+  defp count_distinct?(%{uniq?: true}), do: true
+
+  defp count_distinct?(%{field: nil} = aggregate) do
+    aggregate_filter_references_to_many_relationship?(aggregate)
+  end
+
+  defp count_distinct?(_aggregate), do: false
+
   defp maybe_filter_aggregate(query, aggregate, dynamic) do
     case aggregate.query.filter do
       nil ->
-        {query, dynamic}
+        {:ok, query, dynamic}
 
       %{expression: nil} ->
-        {query, dynamic}
+        {:ok, query, dynamic}
 
       filter ->
-        {filter_dynamic, acc} =
-          AshSql.Expr.dynamic_expr(
-            query,
-            filter,
-            Map.put(query.__ash_bindings__, :location, :aggregate),
-            false
-          )
+        with {:ok, query} <-
+               AshSql.Join.join_all_relationships(
+                 query,
+                 filter,
+                 [],
+                 nil,
+                 [],
+                 nil,
+                 true,
+                 nil,
+                 nil,
+                 true
+               ) do
+          {filter_dynamic, acc} =
+            AshSql.Expr.dynamic_expr(
+              query,
+              filter,
+              Map.put(query.__ash_bindings__, :location, :aggregate),
+              false
+            )
 
-        {AshSql.Bindings.merge_expr_accumulator(query, acc),
-         Ecto.Query.dynamic(filter(^dynamic, ^filter_dynamic))}
+          {:ok, AshSql.Bindings.merge_expr_accumulator(query, acc),
+           Ecto.Query.dynamic(filter(^dynamic, ^filter_dynamic))}
+        end
     end
   end
 
